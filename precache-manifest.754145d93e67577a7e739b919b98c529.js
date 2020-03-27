9d11b18455812e44bb1824efd3158380a34d3ee9
Michał Winiarski <michal.winiarski91@gmail.com>
2020-03-27 14:43:15 +0100

    Updates

---
+++ precache-manifest.754145d93e67577a7e739b919b98c529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb17c8d53f0dc66210f35ed5b9df978a",
+    "url": "/movie-tinder/index.html"
+  },
+  {
+    "revision": "73c092d34e6b4e4faa23",
+    "url": "/movie-tinder/static/css/main.915dc083.chunk.css"
+  },
+  {
+    "revision": "e29c2c3d0e807fd699ac",
+    "url": "/movie-tinder/static/js/2.5ec3a267.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movie-tinder/static/js/2.5ec3a267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c092d34e6b4e4faa23",
+    "url": "/movie-tinder/static/js/main.555afa2e.chunk.js"
+  },
+  {
+    "revision": "c9cd670789c0b2990664",
+    "url": "/movie-tinder/static/js/runtime-main.a9b01e81.js"
+  }
+]);
